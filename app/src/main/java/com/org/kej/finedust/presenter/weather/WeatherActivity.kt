13b26f4936245b87003398f6f7caf6559d8bf598
KwakEuiJin <yuijin0909@gmial.com>
2023-03-14 13:51:15 +0900

    날씨 기능 추가

---
+++ app/src/main/java/com/org/kej/finedust/presenter/weather/WeatherActivity.kt
@@ -0,0 +1,12 @@
+package com.org.kej.finedust.presenter.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.org.kej.finedust.R
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
