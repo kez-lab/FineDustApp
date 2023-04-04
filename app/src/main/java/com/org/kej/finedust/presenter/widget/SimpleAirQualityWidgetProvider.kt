353390cecc007dc002a37bcdffcd5f5d82678071
KwakEuiJin <yuijin0909@gmail.com>
2023-04-04 21:56:05 +0900

    feat(widget): 위젯 기능 추가

---
+++ app/src/main/java/com/org/kej/finedust/presenter/widget/SimpleAirQualityWidgetProvider.kt
@@ -0,0 +1,12 @@
+package com.org.kej.finedust.presenter.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class SimpleAirQualityWidgetProvider: AppWidgetProvider() {
+
+    override fun onUpdate(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+    }
+}
